6d8a8cd561dc03821ba8eb57b714ba4dab798401
Yves Mettier <ymettier@free.fr>
2012-10-31 13:16:26 +0100

    Added variables - updated the website to the new pkg versions

--- genpage.sh
+++ genpage.sh
@@ -2,7 +2,28 @@
 
 pages="index.html screenshots.html compatibility_matrix.html developpement.html faq.html download.html installation.html installation_agent.html installation_server.html installation_frontend.html"
 
+vars=$(cat << EOF
+COLLECTDPW_VERSION=5.1.0-20121031
+COLLECTDPW_PACKAGE=collectd-5.1.0-20121031.tar.gz
+PERFWATCHER_VERSION=1.0
+PERFWATCHER_PACKAGE=perfwatcher-/1.0.20121026.tgz
+EOF
+)
+
+substitute_cmd=$(
+echo "sed "
+for kv in $vars; do
+	k=$(echo "$kv" | cut -d= -f 1)
+	v=$(echo "$kv" | cut -d= -f 2- | sed -e 's/\//\\\//g')
+	echo "-e s/@$k@/$v/g"
+done
+)
+
 for page in $pages
 do
-	cat tpl/header.html tpl/$page tpl/footer.html > $page
+	(
+		cat tpl/header.html
+		cat tpl/$page | $substitute_cmd
+		cat tpl/footer.html
+		) > $page
 done
