0c21902190408b87237b8daaf85257342fec9115
Yves Mettier <ymettier@free.fr>
2013-04-03 14:49:04 +0200

    cleanup in DB creation scripts

--- install/create.sql
+++ install/create.sql
@@ -1,58 +1,14 @@
--- MySQL dump 10.13  Distrib 5.1.61, for redhat-linux-gnu (x86_64)
---
--- Host: localhost    Database: jsTree
--- ------------------------------------------------------
--- Server version	5.1.61-log
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `jsTree`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `perfwatcher` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `perfwatcher`;
-
---
--- Table structure for table `tree`
---
-
-DROP TABLE IF EXISTS `tree`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tree` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `parent_id` bigint(20) unsigned NOT NULL,
-  `position` bigint(20) unsigned NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `datas` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`,`parent_id`),
-  KEY `title` (`title`),
-  KEY `type` (`type`),
-  KEY `id_2` (`id`,`title`)
-) ENGINE=MyISAM AUTO_INCREMENT=116045 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2012-10-25 16:08:18
-
+CREATE TABLE tree (
+  id        bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  parent_id bigint(20) unsigned NOT NULL,
+  position  bigint(20) unsigned NOT NULL,
+  title     varchar(255) DEFAULT NULL,
+  type      varchar(255) DEFAULT NULL,
+  datas     text NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id,parent_id),
+  KEY title (title),
+  KEY type (type),
+  KEY id_2 (id,title)
+);
