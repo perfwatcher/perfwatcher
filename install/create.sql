9510d32e9a88de178430cf40517573f49c74e21d
Yves Mettier <ymettier@free.fr>
2013-12-16 16:15:15 +0100

    Updates on documentation

--- install/create.sql
+++ install/create.sql
@@ -6,14 +6,16 @@ CREATE TABLE tree (
   position  bigint(20) unsigned NOT NULL,
   title     varchar(255) DEFAULT NULL,
   type      varchar(255) DEFAULT NULL,
+  pwtype    varchar(255) DEFAULT NULL,
   datas     text NOT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY id (id,parent_id),
   KEY view_id (view_id),
   KEY title (title),
   KEY type (type),
+  KEY pwtype (pwtype),
   KEY id_2 (id,title)
 );
 
-INSERT INTO tree (id,view_id, parent_id,position,title,type) VALUES (2,1,1,0,'Default view', 'folder');
+INSERT INTO tree (id,view_id, parent_id,position,title,type) VALUES (2,1,1,0,'Default view', 'folder', 'container');
 
