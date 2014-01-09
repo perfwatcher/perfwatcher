f4911c397e3905b3927a739797351e3d52ec26b2
Yves Mettier <ymettier@free.fr>
2014-01-09 14:24:14 +0100

    Moved clipboard code to a dedicated file

--- js/perfwatcher.js
+++ js/perfwatcher.js
@@ -28,6 +28,7 @@ graphid = 0;
 var treecollapsed = false;
 var contextMenu;
 var clipboard = new Array();
+var selection_is_saved = true;
 function positionsubmenu(position, elements) {
 		var options = {
 			of: elements.target.element   
@@ -40,101 +41,6 @@ function positionsubmenu(position, elements) {
 				options.at = "right top";
 		}
 		elements.element.element.position(options);
-}
-
-function clipboard_update_title() {
-	$("#clip_content > span ").text("Clip : "+clipboard.length);
-}
-function clipboard_append(txt) {
-	clipboard.push(txt);
-	clipboard_update_title();
-}
-
-function clipboard_empty() {
-	clipboard = [];
-	clipboard_update_title();
-}
-
-function clipboard_prepare_dialog() {
-    var grouped_types = get_grouped_types();
-    $('#modalcliplist').append("<ul></ul>");
-    $.each(clipboard, function(k,v) {
-        var img = pwmarkdown_filter(v);
-        $('#modalcliplist ul').append(
-            "<li class='ui-state-default'>"
-            +"<div class='clipboard_item'>"
-            +img
-            +"<span class='clipboard_string'>"+v+"</span>"
-            +"<button class='rm_from_clipboard'>Remove from clipboard</button>"
-            +"</div>"
-            +"</li>");
-    });
-    $('#modalcliplist span[class="rrdgraph_to_render"]').each(function(idx) {
-        var item_current = $(this);
-        var code=decodeURIComponent($(this).text());
-        var graph_vars = [];
-        $(this).text(code);
-        try {
-            graph_vars = $.parseJSON(code);
-        } catch(e) {
-            console.log(e);
-            console.log("json was :\n"+code);
-        }
-  
-        var check_grouped_type = jQuery.inArray(graph_vars['type'], grouped_types);
-        var g = {
-            "cdsrc": graph_vars['cdsrc'],
-            "host": graph_vars['host'],
-            "plugin": graph_vars['plugin'],
-            "plugin_instance": graph_vars['plugininstance']?graph_vars['plugininstance']:"",
-            "type": graph_vars['type'],
-            "type_instance": (check_grouped_type == -1)?(graph_vars['typeinstance']?graph_vars['typeinstance']:""):""
-            };
-  
-        var item_graph = $('<img class="graph" id="graph_'+graphid+'" zone="clip"/>');
-        item_graph.insertAfter(item_current);
-        item_graph.pwgraph(g).pwgraph('display');
-        item_current = item_graph;
-  
-        graphid++;
-        $(this).hide();
-    });
-    $('#modalcliplist ul').sortable({ cancel: "img" });
-
-    $('.rm_from_clipboard').click(function () {
-        $(this).parent().parent().remove();
-        $('#timebutton').hide();
-        $('#timespan').hide();
-        $('#datetime').hide();
-    });
-}
-
-function clipboard_refresh_view() {
-    var pwrole = $('#clipboard_switch_markdown_btn').attr('pwrole');
-    if(pwrole == "markdown") {
-        $('#clipboard_switch_markdown_btn').text('Show graphs');
-        $('#modalcliplist ul .clipboard_string').show();
-        $('#modalcliplist ul .rm_from_clipboard').hide();
-        $('#modalcliplist ul img.graph').hide();
-        $('#timebutton').hide();
-        $('#timespan').hide();
-        $('#datetime').hide();
-    } else {
-        $('#clipboard_switch_markdown_btn').text('Show markdown');
-        $('#modalcliplist ul .clipboard_string').hide();
-        $('#modalcliplist ul .rm_from_clipboard').show();
-        $('#modalcliplist ul img.graph').show();
-    }
-}
-
-function clipboard_switch_view() {
-    var pwrole = $('#clipboard_switch_markdown_btn').attr('pwrole');
-    if(pwrole == "graph") {
-        $('#clipboard_switch_markdown_btn').attr('pwrole', 'markdown');
-    } else {
-        $('#clipboard_switch_markdown_btn').attr('pwrole', 'graph');
-    }
-    clipboard_refresh_view();
 }
 
 $(document).ready(function() {
@@ -175,9 +81,6 @@ $(document).ready(function() {
         }
     });
     clipboard_update_title();
-    
-
-
 
 	$('#mainSplitter').jqxSplitter({
 		panels: [{ size: '300px' }],
@@ -210,44 +113,7 @@ $(document).ready(function() {
         $('#timebutton').hide();
         $('#timespan').hide();
         $('#datetime').hide();
-        $('<div id="modalclipcontent"></div>')
-            .html('<div>'
-                +'<div id="modalclipheader">'
-                +'<table><tr>'
-                +'<td><button id="clipboard_rollback_btn">Cancel changes</button></td>'
-                +'<td><button id="clipboard_switch_markdown_btn" pwrole="graph"></button></td>'
-                +'<td><p>This is the contents of your clipboard. You cannot save it. But you can paste it to a selection/tab</p></td>'
-                +'</tr></table>'
-                +'</div>'
-                +'<div id="modalcliplist"></div>'
-                )
-            .dialog({
-                autoOpen: true,
-                appendTo: '#clip',
-                width: '800px',
-                maxHeight: $(window).height() - 50,
-                position: {my: 'right top', at: 'bottom left', of: '#clip' },
-                title: 'Clipboard contents',
-                close: function(event,ui) {
-                    clipboard = [];
-                    $('#modalcliplist span.clipboard_string').each(function(i) {
-                        clipboard.push($(this).text());
-                    });
-                    clipboard_update_title();
-                    pwgraph_current_zone = "tab";
-                    $('#modalclipcontent').html("");
-                    $(this).dialog('destroy').remove();
-                    $('#timebutton').hide();
-                    $('#timespan').hide();
-                    $('#datetime').hide();
-                },
-                open: function(event, ui) {
-                    pwgraph_current_zone = "clip";
-                    clipboard_prepare_dialog();
-                    clipboard_refresh_view();
-                }
-            })
-            .show();
+        clipboard_new_dialog();
         $('#clipboard_rollback_btn').click(function () {
                 $('#modalcliplist').html('');
                 clipboard_prepare_dialog();
@@ -257,6 +123,7 @@ $(document).ready(function() {
                 clipboard_switch_view();
         });
 	});
+
 	$('a[pwmenuid^="menu_"]').click(function () {
 		//console.log($(this).attr("id"));
 		switch($(this).attr("pwmenuid")) {
